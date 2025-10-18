c930f35a84eccb250e1e9dd7454bae1e4b39d2de
nayf <albrqnt1@gmail.com>
2025-10-19 01:48:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,55 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'استراحة البرق نت',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
-  bool _isLoading = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('🌩️ استراحة البرق نت'),
-      ),
-      body: Stack(
-        children: [
-          RefreshIndicator(
-            color: Colors.indigo,
-            onRefresh: () async {
-              final controller = await _controller.future;
-              controller.reload();
-            },
-            child: WebView(
-              initialUrl: 'assets/index.html',
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (controller) {
-                _controller.complete(controller);
-              },
-              onPageFinished: (_) {
-                setState(() {
-                  _isLoading = false;
-                });
-              },
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('🌩️ استراحة البرق نت'),
+        ),
+        body: const Center(
+          child: Text(
+            'مرحباً نايف! ⚡\nالتطبيق يعمل بنجاح',
+            style: TextStyle(fontSize: 24),
+            textAlign: TextAlign.center,
           ),
-          if (_isLoading)
-            const Center(
-              child: CircularProgressIndicator(),
-            ),
-        ],
+        ),
       ),
     );
   }
